667e0be5de28f33baf23f2f6e88f82afaad05023
FrancescoGalanti <galanti_francesco>
2021-01-07 16:30:12 +0100

    working

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,8 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-       
+        <!-- MAIN CSS -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
 
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,22 @@
 {{-- HEADER --}}
 
-<header>
-   header
+<header class="main-header">
+   <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="{{asset('images/logo.png')}}" alt="DC COMIC">
+      </a>
+      <nav class="navbar-nav">
+        <ul>
+          <li>
+            <a href="#">Character</a>
+          </li>
+          <li>
+            <a href="#">Character</a>
+          </li>
+          <li>
+            <a href="#">Character</a>
+          </li>
+        </ul>
+      </nav>
+   </div>
 </header>
