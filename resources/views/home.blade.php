88468a562c574df3296309d2cadbe9cc1845adfa
FrancescoGalanti <galanti_francesco>
2021-01-07 17:28:40 +0100

    working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,30 @@
        @extends('layout.main')
 
        @section('content')
-         <main>
-          CONTENT HERE
+         {{-- MAIN SECTION --}}
+         <main class="wrap-home">
+           {{-- HERO BACKGROUND --}}
+           <section class="general-hero">
+              <div class="container">
+                 <img src="{{asset('images/cover-home.jpg')}}" alt="Teen go lorem">
+              </div>
+           </section>
+           
+           <section class="comics mt-3 mb-3">
+             <div class="container">
+                 {{-- LIST ITEMS --}}
+                <ul class="comics-list">
+                   @foreach($comics as $item)
+                      <li>
+                          <a href="">
+                            <img src="{{$item['image']}}" alt="{{$item['title']}">
+                            <h3>{{$item['title']}}</h3>
+                          </a>
+                      </li>          
+                   @endforeach
+                </ul>
+             </div>
+           </section>
          </main>
        @endsection
 
