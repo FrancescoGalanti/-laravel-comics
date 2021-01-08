ebb7dff01c442c9f18e76acdc3b6968ca29a08b6
FrancescoGalanti <galanti_francesco>
2021-01-08 14:58:50 +0100

    working

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    public function show($id)
+    
+    {
+        $comics = config('comic');
+
+        $comic = [];
+
+        foreach($comics as $item){
+            if($item['id'] == $id){
+                $comic = $item;
+            }
+        }
+    }
+}
