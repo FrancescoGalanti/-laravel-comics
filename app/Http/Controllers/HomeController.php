44a993d7d8c10a4acef4fcb6d3c5e6e3fdefc3f3
FrancescoGalanti <galanti_francesco>
2021-01-10 20:19:55 +0100

    working

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class HomeController extends Controller
 {
@@ -10,6 +12,17 @@ class HomeController extends Controller
     {
        $comics = config('comic');
        
+       foreach( $comics as $key => $item){
+        $slug = Str::slug($item['title'], '-');
+
+        
+        // add a slug and put it on the array
+        $comics[$key]['slug'] = $slug;
+
+
+    }
+
+    dd($comics);
 
        return view('home', compact('comics'));
     }
